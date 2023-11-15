4d216b5273d80af66edd9f3129ebf6bab37babe2
Wba-m3manzzz <137317410+Wba-m3manzzz@users.noreply.github.com>
2023-11-15 16:20:04 +0700

    Mansur Project

---
+++ lib/Login_Page.dart
@@ -0,0 +1,308 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late Color myColor;
+  late Size mediaSize;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool rememberUser = false;
+
+  @override
+  Widget build(BuildContext context) {
+    myColor = Theme.of(context).primaryColor;
+    mediaSize = MediaQuery.of(context).size;
+    return Container(
+      decoration: BoxDecoration(
+        color: myColor,
+        image: DecorationImage(
+          image: AssetImage("image/background.jpg"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            myColor.withOpacity(0.2),
+            BlendMode.dstATop,
+          ),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Positioned(top: 80, child: _buildTop()),
+            Positioned(bottom: 0, child: _buildBottom()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTop() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            Icons.location_on_sharp,
+            size: 100,
+            color: Colors.white,
+          ),
+          Text(
+            "GO LOGIN",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+              letterSpacing: 2,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottom() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: _buildFrom(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFrom() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Welcome",
+          style: TextStyle(
+            color: myColor,
+            fontSize: 32,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        _buildGreyText("Please login with your information"),
+        const SizedBox(height: 60),
+        _buildGreyText("Email Address"),
+        _buildInputField(emailController),
+        const SizedBox(
+          height: 40,
+        ),
+        _buildGreyText("Password"),
+        _buildInputField(passwordController, isPassword: true),
+        const SizedBox(height: 20),
+        _buildRememberForgot(),
+        const SizedBox(height: 20),
+        _buildLoginButton(),
+        const SizedBox(height: 20),
+        _buildOtherLogin(),
+      ],
+    );
+  }
+
+  Widget _buildGreyText(String text) {
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.grey),
+    );
+  }
+
+  Widget _buildInputField(TextEditingController controller,
+      {isPassword = false}) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        suffixIcon: isPassword
+            ? const Icon(Icons.remove_red_eye)
+            : const Icon(Icons.done),
+      ),
+      obscureText: isPassword,
+    );
+  }
+
+  Widget _buildRememberForgot() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Checkbox(
+              value: rememberUser,
+              onChanged: (value) {
+                setState(() {
+                  rememberUser = value!;
+                });
+              },
+            ),
+            _buildGreyText("Remember me"),
+          ],
+        ),
+        TextButton(
+          onPressed: () {},
+          child: _buildGreyText("I forgot my password"),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Email : ${emailController.text}");
+        debugPrint("Password : ${passwordController.text}");
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SecondPage()),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder(),
+        elevation: 20,
+        shadowColor: myColor,
+        minimumSize: const Size.fromHeight(60),
+      ),
+      child: const Text("LOGIN"),
+    );
+  }
+
+  Widget _buildOtherLogin() {
+    return Center(
+      child: Column(
+        children: [
+          _buildGreyText("Or Login With"),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Tab(icon: Image.asset("image/facebook.png")),
+              Tab(icon: Image.asset("image/twitter.png")),
+              Tab(icon: Image.asset("image/google.png")),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  final List<Product> products = [
+    Product(name: 'Televisi', price: 5000000),
+    Product(name: 'Radio', price: 50000),
+    Product(name: 'Remote', price: 15000),
+    Product(name: 'Laptop', price: 10000000),
+    Product(name: 'PC', price: 8000000),
+    Product(name: 'Handphone', price: 2000000),
+    Product(name: 'Kabel', price: 80000),
+    Product(name: 'Batre', price: 200000),
+    Product(name: 'Modem', price: 130000),
+    Product(name: 'Charger', price: 30000),
+    Product(name: 'Speaker', price: 150000),
+    Product(name: 'Mouse', price: 50000),
+    Product(name: 'Keyboard', price: 120000),
+    Product(name: 'Monitor', price: 3000000),
+    Product(name: 'Printer', price: 700000),
+    Product(name: 'Camera', price: 1500000),
+    Product(name: 'Headphones', price: 300000),
+    Product(name: 'Tablet', price: 900000),
+    Product(name: 'Smartwatch', price: 400000),
+    Product(name: 'Drone', price: 2000000),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Data Produk'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          Product product = products[index];
+
+          return Column(
+            children: [
+              ListTile(
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+                title: Text(
+                  '${index + 1}. ${product.name}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                subtitle: Text(
+                  'Harga Rp.${_formatPrice(product.price)}',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    _showDeleteDialog(context, product);
+                  },
+                ),
+              ),
+              const Divider(),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  String _formatPrice(int price) {
+    return price.toString().replaceAllMapped(
+        RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+        (Match match) => '${match[1]},');
+  }
+
+  void _showDeleteDialog(BuildContext context, Product product) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Delete Product'),
+          content: Text('Are you sure you want to delete ${product.name}?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Delete'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Product {
+  final String name;
+  final int price;
+
+  Product({required this.name, required this.price});
+}
